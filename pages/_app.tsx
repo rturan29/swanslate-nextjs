9222f182be0cc103a85d775875bc37618b30f25e
Resul <81697915+rturan29@users.noreply.github.com>
2021-09-04 16:31:08 +0300

    pi auth methodu uygulandı

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 // import { store } from "../redux/store";
-import React from "react";
+import React, { useEffect } from "react";
 // import { Provider } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import theme from "../lib/theme";
@@ -11,10 +11,9 @@ import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-
-
-
-
+  useEffect(() => {
+    (globalThis as any)?.window?.Pi?.init({ version: "2.0" });
+  }, [])
 
   return (
     <>
@@ -26,7 +25,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} />
     </ThemeProvider>
       <Script strategy="beforeInteractive" src="https://sdk.minepi.com/pi-sdk.js"></Script>
-      <Script strategy="afterInteractive" id="Pi-Init">{(globalThis as any)?.window?.Pi?.init({ version: "2.0" })}</Script>
     </>
   );
 }
